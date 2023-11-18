b0209ce285d5d938b888dcd543bda9a767c5814d
scammi <scammi@gmail.com>
2023-11-18 13:39:47 -0300

    Removed unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import { useState } from "react";
 import axios from "axios";
+import { useState } from "react";
 import { BasicIpfsData } from "./api/ipfs";
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [loading, setLoading] = useState(false);
