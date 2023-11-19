f74824b5df895e6abdaa8172eb319c77c6e73fb3
scammi <scammi@gmail.com>
2023-11-18 22:36:13 -0300

    Basic form structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,17 @@ import axios from "axios";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
+import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
 
 const Home = () => {
   const [loading, setLoading] = useState(false);
-  const [CID, setCID] = useState("");
   const [fileName, setFileName] = useState("");
   const [fileContent, setFileContent] = useState("");
+  const enableUpload = !Boolean(fileContent && fileName);
 
   const handleLoad = async () => {
     setLoading(true);
@@ -36,37 +39,44 @@ const Home = () => {
     <Container>
       <main>
         <Grid container justifyContent="center">
-          <Grid item xs={5}>
-            <Paper>
-              <input
-                type="text"
-                placeholder="File name"
-                value={fileName}
-                onChange={(e) => setFileName(e.target.value)}
-              />
-              <br />
-              <input
-                type="text"
-                placeholder="Content"
-                value={fileContent}
-                onChange={(e) => setFileContent(e.target.value)}
-              />
-              <br />
-              <button
-                onClick={handleLoad}
-                className={classNames(
-                  "bg-slate-300 hover:bg-slate-500 text-black rounded-md p-2 drop-shadow-md w-32",
-                  loading ? "animate-pulse" : ""
-                )}
-              >
-                {loading ? "Loading..." : "Retrieve Data"}
-              </button>
-              <br />
-              <button
-                onClick={handlePost}
-              >
-                Upload  
-              </button>
+          <Grid item xs={6}>
+           <Paper elevation={4}>
+              <Stack spacing={2}>
+                <TextField
+                  style={{ margin: "5px" }}
+                  value={fileName}
+                  onChange={(e) => setFileName(e.target.value)}
+                  id="fileNameInput"
+                  label="File name"
+                />
+                <TextField
+                  style={{ margin: "5px" }}
+                  multiline={true}
+                  rows={3}
+                  type="text"
+                  value={fileContent}
+                  onChange={(e) => setFileContent(e.target.value)}
+                  id="fileNameInput"
+                  label="Content"
+                />
+                {/* <button
+                  onClick={handleLoad}
+                  className={classNames(
+                    "bg-slate-300 hover:bg-slate-500 text-black rounded-md p-2 drop-shadow-md w-32",
+                    loading ? "animate-pulse" : ""
+                  )}
+                >
+                  {loading ? "Loading..." : "Retrieve Data"}
+                </button> */}
+                <Button
+                  disabled={enableUpload}
+                  style={{ margin: "5px" }}
+                  variant="outlined"
+                  onClick={handlePost}
+                >
+                  Upload  
+                </Button>
+              </Stack>
             </Paper>
           </Grid>
         </Grid>
