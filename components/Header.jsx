6e896061aefcc8b13cb53d36b49c3043997ac745
scammi <scammi@gmail.com>
2023-11-20 08:31:51 -0300

    Connect wallet

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,9 +11,15 @@ import { createWalletClient, custom } from 'viem';
 import { mainnet } from 'viem/chains';
 
 const AppHeader = () => {
-  useEffect(() => {
-    console.log('asdf')
-  }, []);
+
+  const connect = async () => {
+    const client = createWalletClient({
+      chain: mainnet,
+      transport: custom(window.ethereum)
+    })
+    const [address] = await client.requestAddresses() 
+    console.log(address)
+  };
 
   return (
     <Box sx={{ flexGrow: 1, pb: 5 }}>
@@ -31,7 +37,7 @@ const AppHeader = () => {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
            IPFS Notes 
           </Typography>
-          <Button variant="contained" onClick={() => console.log('Click')}>
+          <Button variant="contained" onClick={async() => await connect()}>
             Connect
           </Button>
         </Toolbar>
